e8fc899f9fcac491e2da97c0ce8fa00c30a8150e
Gamal-prog <atemil791@gmail.com>
2024-06-04 15:31:00 +0600

    forth

--- website/appitself/forms.py
+++ website/appitself/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.forms import TextInput, FloatField, NumberInput
 from .models import Point
 
 class PointForm(forms.ModelForm):
@@ -6,3 +7,21 @@ class PointForm(forms.ModelForm):
     class Meta:
         model = Point
         fields = ('name', 'longitude', 'latitude',)
+
+        widgets = {
+            'name': TextInput(attrs={
+                'class': "form-control",
+                'style': 'max-width: 250px;',
+                'placeholder': 'Name'
+            }),
+            'longitude': NumberInput(attrs={
+                'class': "form-control",
+                'style': 'max-width: 250px;',
+                'placeholder': 'Longitude'
+            }),
+            'latitude': NumberInput(attrs={
+                'class': "form-control",
+                'style': 'max-width: 250px;',
+                'placeholder': 'Latitude'
+            }),
+        }
