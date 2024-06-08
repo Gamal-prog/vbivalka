0f79153c22a0f31c5b45dd5d9d3220ed091ca833
Gamal-prog <atemil791@gmail.com>
2024-06-08 12:54:44 +0600

    scripts in the one file

--- website/appitself/static/appitself/main.js
+++ website/appitself/static/appitself/main.js
@@ -1,5 +1,29 @@
 $(document).ready(function(){
 
+        ymaps.ready(init);
+        function init() {
+            // Display the map on Bishkek city
+            var map = new ymaps.Map("Bishkek map", {
+                center: [42.874621, 74.605438],  // Bishkek's coordinate 
+                zoom: 12 // Scale
+            });
+        
+            // Set the city name for the point
+            var placemark = new ymaps.Placemark([42.874621, 74.605438], {
+                balloonContent: 'Бишкек' 
+            });
+            
+            // Add the name
+            map.geoObjects.add(placemark);
+
+            // Gets datas from click
+            map.events.add('click', function (e) {
+                var coords = e.get('coords');
+                $('#id_latitude').val(coords[0]);
+                $('#id_longitude').val(coords[1]);
+            });
+        }
+
     $('#point_form').submit(function(e) {
         e.preventDefault();  
 
