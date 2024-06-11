726548001e46af32abde46aea6ec33b5192747c9
Gamal-prog <atemil791@gmail.com>
2024-06-11 14:15:31 +0600

    add field in model

--- website/appitself/forms.py
+++ website/appitself/forms.py
@@ -1,18 +1,23 @@
 from django import forms
-from django.forms import TextInput, FloatField, NumberInput
+from django.forms import TextInput, NumberInput
 from .models import Point
 
 class PointForm(forms.ModelForm):
 
     class Meta:
         model = Point
-        fields = ('name', 'longitude', 'latitude',)
+        fields = ('name', 'longitude', 'latitude', 'street',)
 
         widgets = {
             'name': TextInput(attrs={
                 'class': "form-control",
                 'style': 'max-width: 250px;',
                 'placeholder': 'Name'
+            }),
+            'street': TextInput(attrs={
+                'class': "form-control",
+                'style': 'max-width: 250px;',
+                'placeholder': 'Street'
             }),
             'longitude': NumberInput(attrs={
                 'class': "form-control",
--- website/appitself/models.py
+++ website/appitself/models.py
@@ -5,6 +5,7 @@ class Point(models.Model):
 
     #atributes
     name = models.CharField(max_length=100, blank=False)
+    street = models.CharField(max_length=100, blank=False)
     longitude = models.FloatField()
     latitude = models.FloatField()
 
